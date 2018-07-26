672cbf8714ebba4064546e0e52baf7157f10cb68
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-07-26 10:03:25 -0300

    added icons

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# proton
+# Proton
 
-> An electron-vue project
+> A DAW built with Electron and Vue!
 
 #### Build Setup
 
 ``` bash
+# Ensure you have npm installed.
+# I recommend using NVM!
+
 # install dependencies
 npm install
 
@@ -22,7 +25,3 @@ npm test
 npm run lint
 
 ```
-
----
-
-This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue)@[4c6ee7b](https://github.com/SimulatedGREG/electron-vue/tree/4c6ee7bf4f9b4aa647a22ec1c1ca29c2e59c3645) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
--- src/renderer/components/FileExplorer.vue
+++ src/renderer/components/FileExplorer.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="jacob" style="display: contents">
     <v-list-tile @click="showChildren = !showChildren">
+      <v-list-tile-action style="min-width: 40px">
+        <v-icon :style="indent">{{ isLeaf ? 'audiotrack' : 'folder' }}</v-icon>
+      </v-list-tile-action>
       <v-list-tile-content>
         <v-list-tile-title :style="indent">{{ tree.label }}</v-list-tile-title>
-        <!--<v-list-tile-action>-->
-          <!--<v-icon light>compare_arrows</v-icon>-->
-        <!--</v-list-tile-action>-->
-        <!--<v-list-tile-title>Switch drawer (click me)</v-list-tile-title>-->
       </v-list-tile-content>
     </v-list-tile>
     <file-explorer v-if="showChildren" v-for="tree in self.trees" :tree="tree" :depth="depth + 1">
@@ -23,11 +22,14 @@
     },
     computed: {
       indent () {
-        return { transform: `translate(${this.depth * 20}px)` }
+        return { transform: `translate(${this.depth * 10}px)` }
       },
       self () {
         // Doing this removes confusion
         return this.tree
+      },
+      isLeaf () {
+        return !this.tree.trees
       }
     }
   }
