3c30a85acf4325bb0ab5555936b395b47637900c
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-07-26 09:44:46 -0300

    init

---
+++ src/renderer/components/FileExplorer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="display: contents">
+    <v-list-tile @click="showChildren = !showChildren">
+      <v-list-tile-title>{{ tree.label }}</v-list-tile-title>
+      <!--<v-list-tile-action>-->
+        <!--<v-icon light>compare_arrows</v-icon>-->
+      <!--</v-list-tile-action>-->
+      <!--<v-list-tile-title>Switch drawer (click me)</v-list-tile-title>-->
+    </v-list-tile>
+    <file-explorer v-if="showChildren" v-for="tree in self.trees" :tree="tree" :depth="depth + 1">
+    </file-explorer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FileExplorer',
+    props: {tree: Object, depth: {type: Number, default: 0}},
+    data () {
+      return { showChildren: false }
+    },
+    computed: {
+      indent () {
+        return { transform: `translate(${this.depth * 20}px)` }
+      },
+      self () {
+        // Doing this removes confusion
+        return this.tree
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
